f7812fcd6653a6dfa213ec126b1893b8b22fd2c9
lambdarw <115614923+lambdarw@users.noreply.github.com>
2024-12-27 15:21:27 +0800

    Update CreateWindowsBatchScript.cmake

--- MakeFile/QV001/src/CreateWindowsBatchScript.cmake
+++ MakeFile/QV001/src/CreateWindowsBatchScript.cmake
@@ -1,12 +1,3 @@
-#######################################################################
-#
-#  Copyright (c) 张晓东, 罗火灵. All rights reserved.
-#  更多信息请访问: 
-#    http://www.vtkchina.org (VTK中国)
-#	http://blog.csdn.net/www_doling_net (东灵工作室) 
-#
-#######################################################################
-
 FUNCTION(CreateWindowsBatchScript in out build_type)
   IF(VTK_DIR)
     SET(VTK_BIN_DIR "${VTK_DIR}/bin/${build_type}")
